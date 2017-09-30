a435c0147342e2a9499558b3bce050916fdb5d4e
mattspeller <matt.speller@gmail.com>
2017-09-30 22:40:14 +0100

    made the final rename of app.js

--- src/socket-io-client/src/index.js
+++ src/socket-io-client/src/index.js
@@ -2,8 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import SymbolTicker from './components/SymbolTicker.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import GoldenLayout from 'golden-layout'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.React = React
+window.ReactDOM = ReactDOM
+
+var createReactClass = require('create-react-class');
+
+var myLayout = new GoldenLayout({
+    content: [{
+        type: 'row',
+        content:[{
+            type:'react-component',
+            component: 'test-component',
+            props: { label: 'A' }
+        },{
+            type: 'column',
+            content:[{
+                type:'react-component',
+                component: 'test-component',
+                props: { label: 'B' }
+            },{
+                type:'react-component',
+                component: 'test-component',
+                props: { label: 'C' }
+            }]
+        }]
+    }]
+});
+
+
+var TestComponent = createReactClass({
+    render: function() {
+        return (<h1>{this.props.label}</h1>)
+    }
+});
+
+myLayout.registerComponent( 'test-component', SymbolTicker );
+
+//Once all components are registered, call
+myLayout.init();
+
+//ReactDOM.render(<SymbolTicker />, document.getElementById('root'));
 registerServiceWorker();

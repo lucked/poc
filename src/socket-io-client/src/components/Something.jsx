efd95f9038dba733b0853c4971728a4243185861
mattspeller <matt.speller@gmail.com>
2017-09-30 23:16:32 +0100

    set layout as required for POC

---
+++ src/socket-io-client/src/components/Something.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Something extends Component{
+    constructor() {
+        super();
+            this.state = {symbol: ""};
+    }
+
+    Something() {
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <p>Something</p>
+            </div>
+        );
+    }
+};
+
+export default Something;
--- src/socket-io-client/src/index.js
+++ src/socket-io-client/src/index.js
@@ -5,34 +5,51 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import SymbolTicker from './components/SymbolTicker.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import GoldenLayout from 'golden-layout'
+import Something from './components/Something.jsx';
 
 window.React = React
 window.ReactDOM = ReactDOM
-
 
 var myLayout = new GoldenLayout({
     content: [{
         type: 'row',
         content:[{
-            type:'react-component',
-            component: 'test-component',
-            props: { label: 'A' }
-        },{
             type: 'column',
             content:[{
                 type:'react-component',
-                component: 'test-component',
-                props: { label: 'B' }
+                component: 'ticker'
             },{
                 type:'react-component',
-                component: 'test-component',
-                props: { label: 'C' }
+                component: 'something',
+                height: 15
+            }]
+        },
+        {
+            type: 'row',
+            content:[{
+                type: 'column',
+                content:[{
+                    type:'react-component',
+                    component: 'something'
+                }]
+            },
+            {
+                type: 'column',
+                content:[{
+                    type:'react-component',
+                    component: 'something'
+                },{
+                    type:'react-component',
+                    component: 'something'
+                }]
             }]
         }]
     }]
 });
 
-myLayout.registerComponent( 'test-component', SymbolTicker );
+
+myLayout.registerComponent( 'ticker', SymbolTicker );
+myLayout.registerComponent( 'something', Something );
 
 //Once all components are registered, call
 myLayout.init();
